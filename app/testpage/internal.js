fdd60e1d404f4d5407b01d10d57657b09d53a652
smpak19 <smpak@kaist.ac.kr>
2023-05-25 20:27:56 +0900

    Test

--- app/testpage/internal.js
+++ app/testpage/internal.js
@@ -14,13 +14,26 @@ export function Internal() {
   setDefaultChain(sepolia);
 
   const { isConnected, address } = useAccount();
-
+  const [ethBalance, setEthBalance] = useState("0");
   const { data: walletClient } = useWalletClient();
+  const {data: ethBalanceData} = useBalance({
+    address,
+  });
 
   
   const [userAddr, setUserAddress] = useState("");
   const [validUser, setValidUser] = useState("");
   const [info, setInfo] = useState("");
+
+  const [userAddr2, setUserAddress2] = useState("");
+  const [info2, setInfo2] = useState("");
+  const [info3, setInfo3] = useState("");
+
+  const fetchEthBalance = async() => {
+    if (!isConnected) return;
+    const res = ethBalanceData?.formatted;
+    setEthBalance(res);
+  }
 
   // Add user to whiteList
   const addWhiteList = async () => {
@@ -57,7 +70,7 @@ export function Internal() {
   };
 
   // Get whole concert info
-  const getConcertInfo = async() => {
+  const getConcertInfo = async () => {
     if (!isConnected) return;
     const res = await readContract({
       address: TicketNFTFactoryAddress,
@@ -65,24 +78,81 @@ export function Internal() {
       functionName: "getConcertInfo",
     });
     setInfo(res.toString());
-  }
+  };
 
   // Create ticket
-  // 한번 만들면 못없앰 (나중에 리스트 지우는 함수 추가 예정)
   // 만들어진 컨트랙트 주소들 관리
-  const createTicketNFT = async() => {
+  const createTicketNFT = async () => {
     const res = await writeContract({
       address: TicketNFTFactoryAddress,
       abi: TicketNFTFactoryAbi,
       functionName: "createTicketNFT",
       args: ["아이유", 20230525, 1000, 15],
     });
-    alert(`successfully created ticketNFT contract in address ${res.toString()}`)
+    alert(`successfully created ticketNFT contract in address ${res}`)
+  };
+
+  // Test codes for TicketNFT contract
+  // Address for above ticketNFT contract
+  const ticketNFTaddress = "0x6C3D984f137B480667Cd566a9738aE48E2adA6B3";
+
+  // Setting concert finish value true
+  const concertOver = async () => {
+    const res = await writeContract({
+      address: ticketNFTaddress,
+      abi: TicketNFTAbi,
+      functionName: "concertOver",
+    });
+    alert(`Concert over setting done.`);
+  };
+
+  // Array [address], each address has index + 1 tokenId
+  const getUserInfo = async () => {
+    if (!isConnected) return;
+    const res = await readContract({
+      address: ticketNFTaddress,
+      abi: TicketNFTAbi,
+      functionName: "getUserInfo",
+    })
+    setInfo2(res.toString());
+  };
+
+  // Return given user address's tokenId > 1 (balanceOf)
+  // If no token -> 0
+  const balanceOf = async () => {
+    if (!isConnected) return;
+    const res = await readContract({
+      address: ticketNFTaddress,
+      abi: TicketNFTAbi,
+      functionName: "balanceOf",
+      args: [userAddr2],
+    });
+    setInfo3(res.toString());
+  };
+
+  // Buy ticket (max 1)
+  const sellTicket = async () => {
+    const res = await writeContract({
+      address: ticketNFTaddress,
+      abi: TicketNFTAbi,
+      functionName: "sellTicket",
+      args: [userAddr2],
+      value: parseEther("0.015").toString(),
+    });
+    alert(`successfully mint and transfer ticket NFT!`);
+    fetchEthBalance();
   }
+
+  useEffect(() => {
+    if (isConnected) {
+      fetchEthBalance();
+    }
+  }, [isConnected, address]);
 
   return (
     <>
       <Web3Button />
+      <p>Eth balance: {ethBalance}</p>
       <input className="border 5px" value={userAddr} onChange={(e) => setUserAddress(e.target.value)}/>
       <button className="border 5px" onClick={addWhiteList}>Add whitelist</button>
       <button className="border 5px" onClick={removeWhiteList}>Remove from whitelist</button>
@@ -93,6 +163,16 @@ export function Internal() {
       <button className="border 5px" onClick={createTicketNFT}>Create ticket!</button>
       <button className="border 5px" onClick={getConcertInfo}>GetConcertInfo</button>
       <p>{info}</p>
+      <br></br>
+      <br></br>
+      <p>Below is for ticketNFT contract test</p>
+      <p>Test for {ticketNFTaddress}</p>
+      <input className="border 5px" value={userAddr2} onChange={(e) => setUserAddress2(e.target.value)}/>
+      <button className="border 5px" onClick={getUserInfo}>Get all user info</button>
+      <button className="border 5px" onClick={balanceOf}>Given user tokenId</button>
+      <button className="border 5px" onClick={sellTicket}>Buy ticket</button> 
+      <p>All user info: {info2}</p>
+      <p>Given user's tokenId: {info3}</p>
     </>
   )
 }
