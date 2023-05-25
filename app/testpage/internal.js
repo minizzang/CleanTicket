c599831159058c1fa4dcdc932e64cb21687d7fb1
smpak19 <smpak@kaist.ac.kr>
2023-05-25 15:23:01 +0900

    Add some tests

--- app/testpage/internal.js
+++ app/testpage/internal.js
@@ -17,11 +17,82 @@ export function Internal() {
 
   const { data: walletClient } = useWalletClient();
 
-  // .... likewise
+  
+  const [userAddr, setUserAddress] = useState("");
+  const [validUser, setValidUser] = useState("");
+  const [info, setInfo] = useState("");
+
+  // Add user to whiteList
+  const addWhiteList = async () => {
+    const res = await writeContract({
+      address: TicketNFTFactoryAddress,
+      abi: TicketNFTFactoryAbi,
+      functionName: "addWhiteList",
+      args: [userAddr],
+    });
+    alert(`successfully added to whitelist addr: ${userAddr}`);
+  };
+
+  // Remove user from whiteList
+  const removeWhiteList = async () => {
+    const res = await writeContract({
+      address: TicketNFTFactoryAddress,
+      abi: TicketNFTFactoryAbi,
+      functionName: "removeWhiteList",
+      args: [userAddr],
+    });
+    alert(`successfully removed whitelist addr: ${userAddr}`);
+  };
+
+  // Check address validity of user address
+  const verifyUser = async () => {
+    if (!isConnected) return;
+    const res = await readContract({
+      address: TicketNFTFactoryAddress,
+      abi: TicketNFTFactoryAbi,
+      functionName: "checkValidity",
+      args: [userAddr],
+    });
+    setValidUser(res.toString())
+  };
+
+  // Get whole concert info
+  const getConcertInfo = async() => {
+    if (!isConnected) return;
+    const res = await readContract({
+      address: TicketNFTFactoryAddress,
+      abi: TicketNFTFactoryAbi,
+      functionName: "getConcertInfo",
+    });
+    setInfo(res.toString());
+  }
+
+  // Create ticket
+  // 한번 만들면 못없앰 (나중에 리스트 지우는 함수 추가 예정)
+  // 만들어진 컨트랙트 주소들 관리
+  const createTicketNFT = async() => {
+    const res = await writeContract({
+      address: TicketNFTFactoryAddress,
+      abi: TicketNFTFactoryAbi,
+      functionName: "createTicketNFT",
+      args: ["아이유", 20230525, 1000, 15],
+    });
+    alert(`successfully created ticketNFT contract in address ${res.toString()}`)
+  }
 
   return (
     <>
       <Web3Button />
+      <input className="border 5px" value={userAddr} onChange={(e) => setUserAddress(e.target.value)}/>
+      <button className="border 5px" onClick={addWhiteList}>Add whitelist</button>
+      <button className="border 5px" onClick={removeWhiteList}>Remove from whitelist</button>
+      <button className="border 5px" onClick={verifyUser}>Verify user address</button>
+      <p>User {userAddr} is {validUser}</p>
+
+
+      <button className="border 5px" onClick={createTicketNFT}>Create ticket!</button>
+      <button className="border 5px" onClick={getConcertInfo}>GetConcertInfo</button>
+      <p>{info}</p>
     </>
   )
 }
